1cdd0c5eab14d16f261504502b87656db0d18710
rafatokairin <rptokairin@gmail.com>
2024-07-30 20:47:11 -0300

    arrumar erro ()

---
+++ year3/compilers1/LEXICO/teste.c
@@ -0,0 +1,18 @@
+
+if(@)
+{
+a;
+printf("Adeus mundo \"cruel!\"")
+//Oi, eu sou um comentario de linha
+a->[666] += 0x34 + 07 << !2;
+"cadeia" != ’@’ + variavel_nao_declarada;
+/* oi, eu sou um
+comentario de bloco */
+for(;;){$} +45=-78,0X78+08;
+07; // sou octal
+007; // sou inteiro
+0xa; // sou hexa
+0XA; // tambem sou hexa
+}
+comment++;/*Isto eh um
+comentario sem fim...
